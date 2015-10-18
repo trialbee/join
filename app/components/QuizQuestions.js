60195d38c7e55b028288acdead911e21684951ba
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-10-18 08:49:00 +0100

    moved quiz screen headers into QuizQuestions component

--- app/components/QuizQuestions.js
+++ app/components/QuizQuestions.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
 
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import Button from 'react-bootstrap/lib/Button';
+import Fade from 'react-bootstrap/lib/Fade';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+
 import { FullscreenGrid } from 'components/FullscreenGrid';
 import { QuizCard } from 'components/QuizCard';
 
 export class QuizQuestions extends React.Component {
 
     static defaultProps = {
-        questions: 0,
-        currentQuestion: 0,
+        isVisible: false,
         canAnswer: true,
         setCanAnswer: $=> {},
-        onAnswer: $=> {}
+        onAnswer: $=> {},
+        onAbort: $=> {},
+        // quiz data
+        questions: 0,
+        currentQuestion: 0
     };
 
-    requestAnswer = $=> {
+    requestAnswer = e => {
+        e.preventDefault();
+        e.target.blur();
         this.refs.card.requestAnswer();
     }
 
@@ -27,6 +38,7 @@ export class QuizQuestions extends React.Component {
             canAnswer,
             setCanAnswer,
             onAnswer, 
+            onAbort,
             questions, 
             currentQuestion 
         } = this.props;
@@ -35,6 +47,32 @@ export class QuizQuestions extends React.Component {
         if (currentQuestion >= questions.length) {
             currentQuestion-= 1;
         }
+
+        var header = (
+            <Row>
+                <Col xs={6}>
+                    <h3>Quiz</h3>
+                </Col>
+                <Col xs={6} className="text-right">
+                    <Fade in={canAnswer}>
+                        <Button 
+                            bsStyle="primary" 
+                            onClick={this.requestAnswer} >
+                            <Glyphicon glyph="chevron-right" />
+                        </Button>
+                    </Fade>
+                </Col>
+            </Row>
+        );
+
+        var footer = (
+            <Button 
+                bsStyle="danger"
+                onClick={onAbort}
+                block >
+                Abort!
+            </Button>
+        );
 
         var cards = [(
             <QuizCard key={currentQuestion} 
--- app/containers/Quiz.js
+++ app/containers/Quiz.js
@@ -11,24 +11,11 @@ import { saveProfile } from 'services/firebase-service';
 import { setThankyourStatus } from 'actions/app-actions';
 import { setCanAnswer } from 'actions/quiz-actions';
 
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import Button from 'react-bootstrap/lib/Button';
-import Fade from 'react-bootstrap/lib/Fade';
-import Glyphicon from 'react-bootstrap/lib/Glyphicon';
-
-import { FullscreenGrid } from 'components/FullscreenGrid';
 import { QuizQuestions } from 'components/QuizQuestions';
 import { QuizResults } from 'components/QuizResults';
 
 @connect(s => s)
 export class Quiz extends React.Component {
-
-    requestAnswer = e => {
-        e.preventDefault();
-        e.target.blur();
-        this.refs.questions.requestAnswer();
-    }
 
     saveProfile = data => {
         var { dispatch } = this.props;
@@ -49,42 +36,15 @@ export class Quiz extends React.Component {
         var showQuestions = app.isPlaying;
         var showResults = currentQuestion >= questions.length;
 
-        var header = (
-            <Row>
-                <Col xs={6}>
-                    <h3>Quiz</h3>
-                </Col>
-                <Col xs={6} className="text-right">
-                    <Fade in={canAnswer}>
-                        <Button 
-                            bsStyle="primary" 
-                            onClick={this.requestAnswer} >
-                            <Glyphicon glyph="chevron-right" />
-                        </Button>
-                    </Fade>
-                </Col>
-            </Row>
-        );
-
-        var footer = (
-            <Button 
-                bsStyle="danger"
-                onClick={$=> dispatch(abort())}
-                block >
-                Abort!
-            </Button>
-        );
-
         return (
             <div>
                 <QuizQuestions {...quiz}
                     ref="questions"
                     isVisible={showQuestions}
-                    header={header}
-                    footer={footer}
                     canAnswer={canAnswer}
                     setCanAnswer={value => dispatch(setCanAnswer(value))}
-                    onAnswer={data => dispatch(answer(data))} />
+                    onAnswer={data => dispatch(answer(data))} 
+                    onAbort={$=> dispatch(abort())} />
                     
                 <QuizResults {...quiz}
                     isVisible={showResults}
