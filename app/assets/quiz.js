710405abd70e3dfbf5ecbce9e3ffbfad10c8c69a
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-10-26 09:18:06 +0100

    more questions

--- app/assets/quiz.js
+++ app/assets/quiz.js
@@ -5,142 +5,142 @@ export const frontend = [
 
 
 // LANGUAGES
-// {
-//     id: 'Languages',
-//     type: 'info',
-//     question: 'Languages',
-//     html: [
-//         '<p>The following questions are about programming languages.</p>',
-//         '<p>Please answer "yes" to languages that you feel comfortable to work with.</p>'
-//     ].join('')
-// },{
-//     id: 'php',
-//     type: 'bool',
-//     question: 'Do you know PHP?'
-// },{
-//     id: 'ruby',
-//     type: 'bool',
-//     question: 'Do you know Ruby?'
-// },{
-//     id: 'vivaldi',
-//     type: 'bool',
-//     question: 'Do you know Vivaldi?',
-//     helper: [
-//         '<p><a href="https://github.com/jeorgun/Vivaldi" target="_blank">did you know it?</a><br>',
-//         '<a href="https://github.com/jeorgun/Vivaldi/issues/1" target="_blank">we are active contributors!</a></p>'
-//     ].join('')
-// },{
-//     id: 'rails',
-//     type: 'bool',
-//     question: 'Do you know Rails?'
-// },{
-//     id: 'js',
-//     type: 'bool',
-//     question: 'Do you know JavaScript?'
-// },{
-//     id: 'java',
-//     type: 'bool',
-//     question: 'Do you know Java?'
-// },
+{
+    id: 'Languages',
+    type: 'info',
+    question: 'Languages',
+    html: [
+        '<p>The following questions are about programming languages.</p>',
+        '<p>Please answer "yes" to languages that you feel comfortable to work with.</p>'
+    ].join('')
+},{
+    id: 'php',
+    type: 'bool',
+    question: 'Do you know PHP?'
+},{
+    id: 'ruby',
+    type: 'bool',
+    question: 'Do you know Ruby?'
+},{
+    id: 'vivaldi',
+    type: 'bool',
+    question: 'Do you know Vivaldi?',
+    helper: [
+        '<p><a href="https://github.com/jeorgun/Vivaldi" target="_blank">did you know it?</a><br>',
+        '<a href="https://github.com/jeorgun/Vivaldi/issues/1" target="_blank">we are active contributors!</a></p>'
+    ].join('')
+},{
+    id: 'rails',
+    type: 'bool',
+    question: 'Do you know Rails?'
+},{
+    id: 'js',
+    type: 'bool',
+    question: 'Do you know JavaScript?'
+},{
+    id: 'java',
+    type: 'bool',
+    question: 'Do you know Java?'
+},
 
 
 
 
 // CODING
-// {
-//     id: 'Coding',
-//     type: 'info',
-//     question: 'Techniques',
-//     html: [
-//         '<p>Now I will present you with some <b>multiple choices</b> cards.</p>',
-//         '<p>Pick your choice in the <b>shortest amount of time</b>, we track it!</p>'
-//     ].join('')
-// },
-// {
-//     id: 'coding-style',
-//     type: 'multi',
-//     question: 'Pick your choice:',
-//     options: [
-//         {value:'object-oriented',           label: 'Object Oriented'},
-//         {value:'functional-programming',    label: 'Functional Programming'},
-//         {value:'fouxo',                     label: 'FouxO' }
-//     ]
-// },
-// {
-//     id: 'variables',
-//     type: 'multi',
-//     question: 'Pick your choice:',
-//     options: [
-//         {value:'by-reference',              label: 'by reference'},
-//         {value:'by-value',                  label: 'by value'},
-//     ]
-// },
-// {
-//     id: 'indentation',
-//     type: 'multi',
-//     question: 'tabs or spaces?',
-//     helper: 'think it through',
-//     options: [{
-//         value: 'tabs',
-//         label: 'tabs',
-//         style: 'primary'
-//     },{
-//         value: '2spaces',
-//         label: '2 spaces'
-//     },{
-//         value: '4spaces',
-//         label: '4 spaces'
-//     }],
-//     rules: [{
-//         type: 'notEmpty',
-//         msg: 'you must choose an option dumb ass!',
-//         stop: true
-//     },{
-//         type: 'oneOf',
-//         values: ['2spaces', '4spaces'],
-//         msg: 'do you <b>really</b> think <b>tabs</b> is the correct answer???'
-//     }]
-// },
+{
+    id: 'Coding',
+    type: 'info',
+    question: 'Techniques',
+    html: [
+        '<p>Now I will present you with some <b>multiple choices</b> cards.</p>',
+        '<p>Pick your choice in the <b>shortest amount of time</b>, we track it!</p>'
+    ].join('')
+},
+{
+    id: 'coding-style',
+    type: 'multi',
+    question: 'Pick your choice:',
+    options: [
+        {value:'object-oriented',           label: 'Object Oriented'},
+        {value:'functional-programming',    label: 'Functional Programming'},
+        {value:'fouxo',                     label: 'FouxO' }
+    ]
+},
+{
+    id: 'variables',
+    type: 'multi',
+    question: 'Pick your choice:',
+    options: [
+        {value:'by-reference',              label: 'by reference'},
+        {value:'by-value',                  label: 'by value'},
+    ]
+},
+{
+    id: 'indentation',
+    type: 'multi',
+    question: 'tabs or spaces?',
+    helper: 'think it through',
+    options: [{
+        value: 'tabs',
+        label: 'tabs',
+        style: 'primary'
+    },{
+        value: '2spaces',
+        label: '2 spaces'
+    },{
+        value: '4spaces',
+        label: '4 spaces'
+    }],
+    rules: [{
+        type: 'notEmpty',
+        msg: 'you must choose an option dumb ass!',
+        stop: true
+    },{
+        type: 'oneOf',
+        values: ['2spaces', '4spaces'],
+        msg: 'do you <b>really</b> think <b>tabs</b> is the correct answer???'
+    }]
+},
 
 
 
-// {
-//     id: 'personal',
-//     type: 'info',
-//     question: 'Personal',
-//     html: '<p>now we get to know yourself!</p>'
-// },
-// {
-//     id: 'drinks',
-//     type: 'multi',
-//     question: 'Pick your choice:',
-//     options: [
-//         {value:'coffee',            label: 'Coffee'},
-//         {value:'tea',               label: 'Tea'},
-//         {value:'water',             label: 'Water'}
-//     ]
-// },
-// {
-//     id: 'games',
-//     type: 'multi',
-//     question: 'Pick your choice:',
-//     options: [
-//         {value:'fussball',            label: 'Fussball'},
-//         {value:'table-tennis',        label: 'Table Tennis'},
-//         {value:'darts',               label: 'Darts'},
-//         {value:'lightsaber-fight',    label: 'Lightsaber Fight'}
-//     ]
-// },
-// {
-//     id: 'computer',
-//     type: 'multi',
-//     question: 'Pick your choice:',
-//     options: [
-//         {value:'pc',                  label: 'Windows'},
-//         {value:'linux',               label: 'Linux'},
-//         {value:'mac',                 label: 'Mac'}
-//     ]
-// }
+{
+    id: 'personal',
+    type: 'info',
+    question: 'Personal',
+    html: '<p>now we get to know yourself!</p>'
+},
+{
+    id: 'drinks',
+    type: 'multi',
+    question: 'Pick your choice:',
+    options: [
+        {value:'coffee',            label: 'Coffee'},
+        {value:'tea',               label: 'Tea'},
+        {value:'water',             label: 'Water'}
+    ]
+},
+{
+    id: 'games',
+    type: 'multi',
+    question: 'Pick your choice:',
+    options: [
+        {value:'fussball',            label: 'Fussball'},
+        {value:'table-tennis',        label: 'Table Tennis'},
+        {value:'darts',               label: 'Darts'},
+        {value:'lightsaber-fight',    label: 'Lightsaber Fight'}
+    ]
+},
+{
+    id: 'computer',
+    type: 'multi',
+    question: 'Pick your choice:',
+    options: [
+        {value:'pc',                  label: 'Windows'},
+        {value:'linux',               label: 'Linux'},
+        {value:'mac',                 label: 'Mac'}
+    ]
+},
 
 
 
@@ -155,6 +155,61 @@ export const frontend = [
         '<p>Now we get to the real stuff!</p>',
         '<p>In order to answer the following questions you may need to read or even run some code!</p>'
     ].join('')
+},
+{
+    id: 'node-version',
+    type: 'text',
+    question: 'Which version of NodeJS do we use in this app?',
+    rules: [{
+        type: 'oneOf', values: ['0.12.7','12.7']
+    }],
+    hints: [{
+        count: 1,
+        andup: true,
+        msg: 'you may want to <a href="https://github.com/marcopeg/trialbee-hiring" target="_blank">take a look at our codebase</a> to seek for an answert'
+    },{
+        count: 2,
+        andup: true,
+        msg: 'you may find those kind of info around the <b>readme file</b>...'
+    }]    
+},
+{
+    id: 'building-tool',
+    type: 'multi',
+    question: 'Which building tool does this app use?',
+    options: [
+        { value: 'browserify', label:'Browserify' },
+        { value: 'grunt', label:'Grunt' },
+        { value: 'gulp', label:'Gulp' },
+        { value: 'webpack', label:'Webpack' },
+        { value: 'make', label:'Make' }
+    ],
+    rules: [{
+        type: 'oneOf', values: ['webpack']
+    }],
+    hints: [{
+        count: 1,
+        andup: true,
+        msg: 'you may want to <a href="https://github.com/marcopeg/trialbee-hiring" target="_blank">take a look at our codebase</a> to seek for an answert'
+    }]
+},
+{
+    id: 'profile',
+    type: 'text',
+    question: 'How many components does this app have?',
+    rules: [{
+        type: 'oneOf',
+        values: ['13']
+    }],
+    hints: [{
+        count: 1,
+        andup: true,
+        msg: 'you may want to <a href="https://github.com/marcopeg/trialbee-hiring" target="_blank">take a look at our codebase</a> to seek for an answert'
+    },{
+        count: 2,
+        andup: true,
+        msg: 'try to dig deeper into the codebase, <b>we like to use explicit folder names!</b>'
+    }]
 },
 {
     id: 'profile',
@@ -177,7 +232,6 @@ export const frontend = [
         msg: 'the best place to seek for help may be the <b>tests source files</b>!'
     }]
 }
-
 
 
 ];
