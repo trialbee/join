20c4541d9791d078cee63185a8d7e7eaf7e5e9de
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-10-27 11:01:42 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 # Deploy ./dist to gh-pages branch
 
+mkdir ../join-deploy
+cd ../join-deploy
+git clone git@github.com:trialbee/join.git
+cd join
+git checkout gh-pages
 
+# cp -r ../dist/* ./join 
